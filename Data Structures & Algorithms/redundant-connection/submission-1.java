2cfe4afbc8ac14214793c5000e4bf2386c21af8a
RitikaB <137435753+ThisIsRitika@users.noreply.github.com>
2026-09-12 16:41:00 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.java
@@ -0,0 +1,54 @@
+class Solution {
+    class DSU{
+        int[] parent;
+        int[] rank;
+
+        public DSU(int n){
+            parent=new int[n+1];
+            rank=new int[n+1];
+
+            for(int i=1;i<n+1;i++){
+                parent[i]=i;
+                rank[i]=1;
+            }
+        }
+
+        public int find(int node){
+            int curr=node;
+            while(curr!=parent[curr]){
+                parent[curr]=parent[parent[curr]];
+                curr=parent[curr];
+            }
+
+            return curr;
+        }
+
+        public boolean union(int u,int v){
+            int pu=find(u);
+            int pv=find(v);
+
+            if(pu==pv) return false;
+
+            if(rank[pv]>rank[pu]){
+                int temp=pu;
+                pu=pv;
+                pv=temp;
+                //return true;
+            }
+
+            parent[pv]=pu;
+            rank[pu]+=rank[pv];
+            return true;
+        }
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        int n=edges.length;
+        DSU dsu=new DSU(n);
+        int[] ans=new int[2];
+        for(int edge[] : edges){
+            if(!dsu.union(edge[0],edge[1])) ans=edge;
+        }
+
+        return ans;
+    }
+}
